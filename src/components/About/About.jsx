c6b35ced56a0adc2c7dab6fd26b4a981ebac1b4a
Efeakpor <teefwesh0@gmail.com>
2023-01-23 23:30:03 +0100

    update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -14,7 +14,7 @@ const About = ({ theme }) => {
   return (
     <section className="about section" id="about">
       <h2 className="section__title">About Me</h2>
-      <span className="section__subtitle">My Introduction</span>
+      <span className="section__subtitle"> Introduction</span>
 
       <div className="about__container container grid">
         <img
