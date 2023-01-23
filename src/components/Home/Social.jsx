431d5ce2565d1ce72bd6af8a52a18042aadc3376
Efeakpor <teefwesh0@gmail.com>
2023-01-23 23:20:05 +0100

    update

--- src/components/Home/Social.jsx
+++ src/components/Home/Social.jsx
@@ -3,10 +3,20 @@ import React from "react";
 const Social = () => {
   return (
     <div className="home__social">
-      <a href="/" className="home__social-icon" target="_blank">
+      <a
+        href="https://www.instagram.com/invites/contact/?i=48lc6y2f8dm9&utm_content=n9iqs2d"
+        className="home__social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
         <i className="uil uil-instagram"></i>
       </a>
-      <a href="/" className="home__social-icon" target="_blank">
+      <a
+        href="https://www.linkedin.com/in/efeakpor-oghenetega-3379a0124"
+        className="home__social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
         <i className="uil uil-linkedin"></i>
       </a>
       <a href="/" className="home__social-icon" target="_blank">
